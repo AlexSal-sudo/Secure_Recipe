93f52dd443fa15cd4c34d03bcbd8708374d877ee
AlexSal-sudo <slrlsn00r12d086f@studenti.unical.it>
2022-11-24 17:04:30 +0100

    DJANGO Setup

--- Secure_Recipe_Django/urls.py
+++ Secure_Recipe_Django/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+API_TITLE = 'Secure Recipe'
+API_DESCRIPTION = 'A Web API for our Secure project'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-4FeNT*2eT6Dy/', admin.site.urls),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
+    path('schema/', get_schema_view(title=API_TITLE))
 ]
