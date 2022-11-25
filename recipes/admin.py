bd22a5f49c06f7c23a6e528e7fe453797dd804db
Cristian Porco <75568774+Cristian-Porco@users.noreply.github.com>
2022-11-25 15:22:37 +0100

    Push Models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from recipes.models import Ingredient
+from recipes.models import Recipe
 
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    product = models.CharField(max_lenght=25)
+    quantity = models.PositiveIntegerField(default=0)
+
+
 class Recipe(models.Model):
-    pass
+    title = models.CharField(max_lenght=50)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    ingredients = ArrayField(Ingredient)
+    steps = ArrayField(models.CharField(max_length=160))
+
+    def __str__(self):
+        return self.title
