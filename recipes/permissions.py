2609925caf59f3b1e003fdb7bbb5875c808690c3
AlexSal-sudo <slrlsn00r12d086f@studenti.unical.it>
2022-12-05 19:01:21 +0100

    Permission updated

--- recipes/permissions.py
+++ recipes/permissions.py
@@ -1,24 +1,26 @@
 from rest_framework import permissions
 
+method2permit = {
+    'POST': 'add'
+}
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'DELETE':
-            return request.user.is_superuser
+        # if request.method == 'DELETE':
+        #     return request.user.is_superuser
         return True
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or (request.user.is_superuser and request.method == 'DELETE'):
+        if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.author == request.user
+        return False
 
 
 class IsModeratorOrSuperUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.groups.filter(name='recipe_moderators').exists():
-            print("QUI")
-            return True
+        if request.method == 'DELETE':
+            return request.user.groups.filter(name='recipe_moderators').exists()
         return False
